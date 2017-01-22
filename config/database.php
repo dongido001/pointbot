557cbe220d85be74581591ff77b16383770fcada
Gideon <dongidomed@gmail.com>
2017-01-22 14:52:17 +0100

    sdf

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
     // Database driver. Supported: mysql.
     // -------------------------------------------------
 
-    'driver' => 'pgsql',
+    'driver' => 'mysql',
 
     // -------------------------------------------------
     // Should the database component use eloquent?
@@ -19,10 +19,10 @@ return [
     // -------------------------------------------------
 
     'mysql' => [
-        'host'      => 'localhost',
-        'database'  => 'pointbot',
-        'username'  => 'root',
-        'password'  => '',
+        'host'      => 'sql8.freesqldatabase.com',
+        'database'  => 'sql8155055',
+        'username'  => 'sql8155055',
+        'password'  => '9C85JKYVR7',
         'charset'   => 'utf8',
         'collation' => 'utf8_unicode_ci',
         'prefix'    => '',
@@ -35,16 +35,6 @@ return [
             'port' => 6379,
             'database' => 0,
         ],
-    ],
+    ]
 
-    'pgsql' => [
-           'driver'   => 'pgsql',
-           'host'     => 'ec2-54-225-67-3.compute-1.amazonaws.com:5432',
-           'database' => 'dfib33mv04c9jv',
-           'username' => 'hxirqjcpcwriwt',
-           'password' => 'a703aacb06b33e68fd08497cd99b3fed912c014475a83c2c8a35dc20b32803ed',
-           'charset'  => 'utf8',
-           'prefix'   => '',
-           'schema'   => 'public',
-      ],
 ];
