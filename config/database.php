f68834c710cbead68a5530ea212b2851eeedddb5
Gideon <dongidomed@gmail.com>
2017-01-22 13:54:51 +0100

    sdf

--- config/database.php
+++ config/database.php
@@ -6,7 +6,7 @@ return [
     // Database driver. Supported: mysql.
     // -------------------------------------------------
 
-    'driver' => 'mysql',
+    'driver' => 'pgsql',
 
     // -------------------------------------------------
     // Should the database component use eloquent?
@@ -35,5 +35,16 @@ return [
             'port' => 6379,
             'database' => 0,
         ],
-    ]
+    ],
+
+    'pgsql' => [
+           'driver'   => 'pgsql',
+           'host'     => 'ec2-54-225-67-3.compute-1.amazonaws.com:5432',
+           'database' => 'dfib33mv04c9jv',
+           'username' => 'hxirqjcpcwriwt',
+           'password' => 'a703aacb06b33e68fd08497cd99b3fed912c014475a83c2c8a35dc20b32803ed',
+           'charset'  => 'utf8',
+           'prefix'   => '',
+           'schema'   => 'public',
+      ],
 ];
